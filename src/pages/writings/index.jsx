61122cfe874ada1307f28ac1f0871558cc801539
alexjamesgodfrey <alexjamesgodfrey@gmail.com>
2022-09-16 10:01:48 -0400

    Basic website complete

--- src/pages/writings/index.jsx
+++ src/pages/writings/index.jsx
@@ -38,15 +38,12 @@ export default function WritingsIndex({ writings }) {
   return (
     <>
       <Head>
-        <title>writings - Alex Godfrey</title>
-        <meta
-          name="description"
-          content="All of my long-form thoughts on programming, leadership, product design, and more, collected in chronological order."
-        />
+        <title>Writings - Alex Godfrey</title>
+        <meta name="description" content="Some writings" />
       </Head>
       <SimpleLayout
-        title="Writing on software design, company building, and the aerospace industry."
-        intro="All of my long-form thoughts on programming, leadership, product design, and more, collected in chronological order."
+        title="Some writings"
+        intro="I have made for my classes at Cornell."
       >
         <div className="md:border-l md:border-zinc-100 md:pl-6 md:dark:border-zinc-700/40">
           <div className="flex max-w-3xl flex-col space-y-16">
