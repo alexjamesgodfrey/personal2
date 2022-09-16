f53100603ab1b15c0b93312f1269367e1e97aae1
alexjamesgodfrey <alexjamesgodfrey@gmail.com>
2022-09-16 09:06:10 -0400

    Add newsletter

---
+++ src/components/About/Newletter.jsx
@@ -0,0 +1,37 @@
+import { Button } from '@/components/Button'
+import MailIcon from '@/components/Icons/EmailIcon'
+
+export default function Newsletter() {
+  return (
+    <form
+      className="rounded-2xl border border-zinc-100  p-6 dark:border-zinc-700/40"
+      action="https://app.us10.list-manage.com/subscribe/post"
+      target="_blank"
+      method="POST"
+    >
+      <h2 className="flex text-sm font-semibold text-zinc-900 dark:text-zinc-100">
+        <MailIcon className="h-6 w-6 flex-none" />
+        <span className="ml-3">Stay up to date</span>
+      </h2>
+      <p className="mt-2 text-sm text-zinc-600 dark:text-zinc-400">
+        Get notified when I publish something new, and unsubscribe at any time.
+      </p>
+      <div className="mt-6 flex">
+        <input type="hidden" name="u" value="1615cd2f3e299b7a2151bcc37" />
+        <input type="hidden" name="id" value="b3b99f335e" />
+        <input
+          type="email"
+          autoCapitalize="off"
+          autoCorrect="off"
+          name="MERGE0"
+          id="MERGE0"
+          placeholder="Email Address"
+          className="min-w-0 flex-auto appearance-none rounded-md border border-zinc-900/10 bg-white px-3 py-[calc(theme(spacing.2)-1px)] shadow-md shadow-zinc-800/5 placeholder:text-zinc-400 focus:border-primary focus:outline-none focus:ring-4 focus:ring-primary/10 dark:border-zinc-700 dark:bg-zinc-700/[0.15] dark:text-zinc-200 dark:placeholder:text-zinc-500 dark:focus:border-primary-dark dark:focus:ring-primary-dark/10 sm:text-sm"
+        />
+        <Button type="submit" className="ml-4 flex-none">
+          Join
+        </Button>
+      </div>
+    </form>
+  )
+}
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,6 +3,7 @@ import Image from 'next/future/image'
 import Head from 'next/head'
 import Link from 'next/link'
 
+import Newsletter from '@/components/About/Newletter'
 import { Button } from '@/components/Button'
 import { Card } from '@/components/Card'
 import { Container } from '@/components/Container'
@@ -110,35 +111,6 @@ function SocialLink({ icon: Icon, ...props }) {
     <Link className="group -m-1 p-1" {...props}>
       <Icon className="h-6 w-6 fill-zinc-500 transition group-hover:fill-zinc-600 dark:fill-zinc-400 dark:group-hover:fill-zinc-300" />
     </Link>
-  )
-}
-
-function Newsletter() {
-  return (
-    <form
-      action="/thank-you"
-      className="rounded-2xl border border-zinc-100  p-6 dark:border-zinc-700/40"
-    >
-      <h2 className="flex text-sm font-semibold text-zinc-900 dark:text-zinc-100">
-        <MailIcon className="h-6 w-6 flex-none" />
-        <span className="ml-3">Stay up to date</span>
-      </h2>
-      <p className="mt-2 text-sm text-zinc-600 dark:text-zinc-400">
-        Get notified when I publish something new, and unsubscribe at any time.
-      </p>
-      <div className="mt-6 flex">
-        <input
-          type="email"
-          placeholder="Email address"
-          aria-label="Email address"
-          required
-          className="min-w-0 flex-auto appearance-none rounded-md border border-zinc-900/10 bg-white px-3 py-[calc(theme(spacing.2)-1px)] shadow-md shadow-zinc-800/5 placeholder:text-zinc-400 focus:border-primary focus:outline-none focus:ring-4 focus:ring-primary/10 dark:border-zinc-700 dark:bg-zinc-700/[0.15] dark:text-zinc-200 dark:placeholder:text-zinc-500 dark:focus:border-primary-dark dark:focus:ring-primary-dark/10 sm:text-sm"
-        />
-        <Button type="submit" className="ml-4 flex-none">
-          Join
-        </Button>
-      </div>
-    </form>
   )
 }
 
@@ -280,22 +252,30 @@ export default function Home({ writings }) {
           <div className="mt-6 flex gap-6">
             <SocialLink
               href={process.env.NEXT_PUBLIC_GITHUB_LINK || '#'}
+              target="_blank"
+              rel="noreferer noopener"
               aria-label="See my code on GitHub"
               icon={GitHubIcon}
             />
             <SocialLink
               href={process.env.NEXT_PUBLIC_LINKEDIN_LINK || '#'}
+              target="_blank"
+              rel="noreferer noopener"
               aria-label="Connect with me on LinkedIn"
               icon={LinkedInIcon}
             />
             <SocialLink
               href={process.env.NEXT_PUBLIC_INSTAGRAM_LINK || '#'}
+              target="_blank"
+              rel="noreferer noopener"
               aria-label="Follow me on Instagram"
               icon={InstagramIcon}
             />
             <SocialLink
               href={process.env.NEXT_PUBLIC_MAILTO_LINK || '#'}
-              aria-label="Email mes"
+              target="_blank"
+              rel="noreferer noopener"
+              aria-label="Email me"
               icon={EmailIcon}
             />
           </div>
