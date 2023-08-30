44be9fd93b0c66080b11c8e9b9444ddfd64c7fb4
Alex Godfrey <me@alexgodfrey.com>
2023-08-30 06:32:30 -0400

    Add shortcut

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -140,7 +140,7 @@ export default function About() {
                 icon={LinkedInIcon}
                 className="mt-4"
               >
-                Connect on LinkedIn
+                Connect with me on LinkedIn
               </SocialLink>
               <SocialLink
                 href={process.env.NEXT_PUBLIC_INSTAGRAM_LINK}
